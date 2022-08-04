e3374378db8979cb36ff64a01c7deece9984f511
Somdotta07 <jhilick.s@gmail.com>
2022-08-04 18:53:32 +0200

    First commit with basic setup with server and other dependencies, like Bootstrap, react-router-dom

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      &copy; 2022, myVideos. All rights reserved.
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ client/src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <header>
+      vi-Stream
+    </header>
+  );
+}
+
+export default Header;
